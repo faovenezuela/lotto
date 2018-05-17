d3ceb1bb41a06dfb0fef33fe712d2b1659eb8a22
Francisco Alvarez <faovenezuela@gmail.com>
2018-05-17 18:07:05 -0400

    Cerrar sesion

---
+++ logout.php
@@ -0,0 +1,11 @@
+<meta http-equiv="refresh" content="4;url=http://localhost/lotto/" />
+<?php
+session_start();
+
+session_destroy();
+
+//header( "Status: 301 Moved Permanently", false, 301);
+//header( "location: http://www.dissoft.info/sgrm/index.php");
+
+exit();
+?>
