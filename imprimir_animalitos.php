28df641a75d1a6c64908305e6a56549ac54d821f
Francisco Alvarez <faovenezuela@gmail.com>
2018-05-17 18:10:00 -0400

    Funcion que se encarga de imprimir el ticket

---
+++ imprimir_animalitos.php
@@ -0,0 +1,18 @@
+<?php
+//-----------------------------------------------------
+//Funcion que se encarga de imprimir el ticket
+//-----------------------------------------------------
+if (!is_null($imprimir)){
+	
+	//Variable que indica si debo insertar
+	$deboimprimir=0;
+	
+	//Var imprimir suma el siguiente serial de control
+	$varimprimir = $varimprimir+1;
+	//-----------------------------------------------------
+	//Elaborando el serial	
+	//-----------------------------------------------------
+	//$nuevoserial="".date(Y)."".date(m). "".date(d)."" .date(H). "".date(mi)."" .$varimprimir. "";
+	
+}
+?>
